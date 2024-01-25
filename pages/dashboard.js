5c6fa8d2bde5ec6f22acdd7eccf1b1ee7bc84275
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-01-25 16:21:05 +0100

    dashboard page created

---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+// pages/dashboard.js
+import React from 'react';
+import DashboardLayout from '../components/DashboardLayout';
+
+const Dashboard = () => {
+  return <DashboardLayout />;
+    <div>
+      <h1>Hello, World! This is the ONEGLASS.io Dashboard</h1>
+    </div>
+};
+
+export default Dashboard;
