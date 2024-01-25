6acab4a91596169b502ffeb4acf8dcce5a5efddf
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-01-25 17:47:34 +0100

    added api for forecast and inventory

---
+++ api/forecasts.js
@@ -0,0 +1,8 @@
+// api/forecasts.js
+const express = require('express');
+const router = express.Router();
+const { Forecast } = require('../models');
+
+// Define endpoints to fetch forecasted sales
+
+module.exports = router;
---
+++ api/inventory.js
@@ -0,0 +1,8 @@
+// api/inventory.js
+const express = require('express');
+const router = express.Router();
+const { Inventory } = require('../models');
+
+// Define endpoints for managing inventory
+
+module.exports = router;
